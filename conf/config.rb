91abfc2dc383747c5db7a4d44400ea410351229f
Masayoshi Wada <developer@andantesoftware.com>
2013-01-03 22:50:29 +0900

    logファイル消去
    confに注釈追加

--- conf/config.rb
+++ conf/config.rb
@@ -1,12 +1,17 @@
 # config.rb
 
 $config = [{
-  :token => '',
-  :secret => '',
-  :domain => '',
+  :token => 'Gehirn DNS API Token',
+  :secret => 'Gehirn DNS API Secret',
+  :domain => 'example.com',
   :records => {
-    '' => {
-      :ip => nil
+    'example.com.' => {
+      :ip => nil, # nil: auto
+      :hostname => ''
+    },
+    'abc.example.com.' => {
+      :ip => '123.45.67.89', # Appointing an IP address 
+      :hostname => 'abc'
     }
   }
 }]
